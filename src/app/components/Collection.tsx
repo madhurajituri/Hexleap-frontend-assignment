b4f3a29a277dc0568579c18058fa97c34ed67007
madhurajituri <jiturimadhura@gmail.com>
2024-03-27 16:38:35 +0530

    completed assignment

--- src/app/components/Collection.tsx
+++ src/app/components/Collection.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+"use client";
+import React, { useRef } from 'react'
 import arrow from '../assets/Vector.png'
 import { StaticImageData } from 'next/image'
 import ticket1 from '../assets/ticket1.png'
 import ticket2 from '../assets/ticket2.png'
+import { A11y, Autoplay, Navigation, Pagination, Scrollbar, Virtual } from "swiper/modules";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 interface ticketType {
     imageUrl: StaticImageData,
@@ -14,6 +20,9 @@ interface ticketType {
     collection: string
 }
 function Collection() {
+    const swiperRef = useRef(null);
+    const swiper = useSwiper();
+
     let tickets: ticketType[] = [
         {
             imageUrl: ticket1,
@@ -41,6 +50,15 @@ function Collection() {
             time: "4.30 PM",
             address: "Las Vegas Ballpark, Las Vegas, Nevada",
             collection: "Take Flight Collection"
+        },
+        {
+            imageUrl: ticket1,
+            name: "Las Vegas Aviators",
+            date: "OCT 15",
+            day: "SUN",
+            time: "4.30 PM",
+            address: "Las Vegas Ballpark, Las Vegas, Nevada",
+            collection: "Take Flight Collection"
         }
     ]
     return (
@@ -48,24 +66,53 @@ function Collection() {
             <div className='w-[1085.75px] h-full'>
                 <div className='heading font-bold text-[50px] dark:text-white text-center leading-[52.5px]'>Collection Spotlight</div>
                 <div className='mt-[25px] mx-[161px] px-4 dark:text-white Inter font-[400] text-[14px] leading-[22.65px] text-center'>Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable experience. Grab yours today!</div>
-                <div className='mt-[51px] w-full flex items-center justify-between'>
-                    <div className='h-[49px] w-[36.75px] border-[#2C9CF0] border-[1px] flex items-center justify-center'>
-                        <img src={arrow.src} />
+                
+                <div className='gap-[81.25px] flex justify-center items-center'>
+                    <div onClick={swiperRef.current?.swiper.slidePrev()} className="h-[49px] w-[36.75px] border-[#2C9CF0] border-[1px] flex items-center justify-center text-[20px] hover:bg-white"><img src={arrow.src} /></div>
+                    <div className='flex justify-center'>
+
+                        <div className='mt-[51px] w-[850px] relative'>
+                            <Swiper modules={[Navigation]}
+                                spaceBetween={43}
+                                slidesPerView={1}
+                                breakpoints={{
+                                    640: { slidesPerView: 1 },
+                                    768: { slidesPerView: 2 },
+                                    1000: { slidesPerView: 3 }
+                                }}
+                                loop={true}
+                                ref={swiperRef}
+                            >
+
+                                {tickets.map((item, index2) => {
+                                    return <SwiperSlide key={index2}>
+                                        <div className='w-[257px] h-[624px] dark:bg-[#3B3E47] shadow-[0_10px_20px_0_#00000033] flex pt-[15.92px] px-[15.5px] py-[15px] pb-[10.17px]'>
+                                            <div className='flex flex-col items-center'>
+                                                <img src={item.imageUrl.src} className='h-[401.25px]' />
+                                                <div className='relative px-[7.5px] my-[16px] w-[211px] border-t-[0.96px] border-[#818A97] border-dashed'>
+                                                    <div className='w-5 h-5 rounded-full absolute -left-8 -top-2 bg-[#221A2C]'></div>
+                                                    <div className='w-5 h-5 rounded-full absolute -right-8 -top-2 bg-[#221A2C]'></div>
+                                                </div>
+                                                <div className='text-center dark:text-white'>
+                                                    <div className='heading font-[500] leading-[26.52px] text-[17px]'>{item.name}</div>
+                                                    <div className='heading mt-[10px] text-[14px] tracking-tight justify-center text-[#DFDFDF]'>
+                                                        <div>{item.date} | {item.day} | {item.time}</div>
+                                                    </div>
+                                                    <div className='mt-[14px] Inter font-[400] leading-[20.58px] text-[14px] text-[#525965] dark:text-[#DFDFDF]'>{item.address}</div>
+                                                </div>
+                                                <div className='mt-[16px] bg-black w-[213px] h-[36px] items-center justify-center flex hover:bg-zinc-900'>
+                                                    <div className='Inter font-[500] text-[12.73px] leading-[15.41px] text-white'>{item.collection}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                })}
+                            </Swiper>
+                        </div>
                     </div>
-                    <div className='flex justify-between gap-[43px]'>
-                        {tickets.map((item, index2) => {
-                            return <div className='w-[257px] h-[624px] dark:bg-[#3B3E47] shadow-[0_10px_20px_0_#00000033] flex pt-[15.92px] px-[15.5px] py-[15px] pb-[10.17px]' key={index2}>
-                                <div className='flex flex-col'>
-                                    <img src={item.imageUrl.src} className='h-[401.25px]' />
-                                    <div className='mx-[7.5px] my-[20px] border-t-[0.96px] border-[#818A97] border-dashed border-'></div>
-                                </div>
-                            </div>
-                        })}
-                    </div>
-                    <div className='h-[49px] w-[36.75px] border-[#2C9CF0] border-[1px] flex items-center justify-center'>
-                        <img src={arrow.src} className='rotate-180' />
-                    </div>
+                    <div onClick={swiperRef.current?.swiper.slideNext()} className=" h-[49px] w-[36.75px] border-[#2C9CF0] border-[1px] flex items-center justify-center text-[20px] hover:bg-white"><img src={arrow.src} className='rotate-180' /></div>
                 </div>
+
             </div>
         </div>
     )
--- src/app/components/Sports.tsx
+++ src/app/components/Sports.tsx
@@ -39,16 +39,17 @@ function Sports() {
         }
     ]
     return (
-        <div>
+        <div className='relative'>
             <div className='text-2xl leading-9 font-bold heading dark:text-white'>Sports</div>
+            <div className='absolute right-0 top-0 text-base dark:text-white border-2 dark:bg-black border-slate-600 px-3 h-10 rounded-sm'></div>
             <div className='mt-[5px] w-[80.52px] h-0 border-2 border-[#738FFF]'></div>
-            <div className='card my-[25px] flex items-center justify-center gap-[14.09px]'>
+            <div className='card my-[25px] justify-items-center gap-[14.09px] grid grid-cols-1 min-[560px]:grid-cols-2 min-[940px]:grid-cols-3 min-[1190px]:grid-cols-4 min-[1400px]:grid-cols-5'>
                 {
                     cardContent.map((item, index) => {
                         return <div className='w-[238.53px] h-[511px] shadow-[0_8px_16px_0_rgba(0,0,0,0.05)] py-[10px] px-[10.06px] dark:bg-[#3B3E47] dark:text-white' key={index}>
                             <img className='h-[385px] w-[387.48px]' src={item.imageUrl.src} />
-                            <div className='mt-[14.69px] Inter font-[500] text-[17px] leading-[20.57px]'>{item.heading}</div>
-                            <div className='w-[218.4px] h-[54px] mt-4 flex gap-[39.25px] px-[10.06px] py-3 bg-[#F7F7F8] dark:bg-[#292B32]'>
+                            <div className='mt-[12px] Inter font-[500] text-[17px] leading-[20.57px]'>{item.heading}</div>
+                            <div className='w-[218.4px] h-[54px] mt-3 flex items-center gap-[32px] px-[10.06px] py-2 bg-[#F7F7F8] dark:bg-[#292B32]'>
                                 <div className='flex flex-col gap-[12px]'>
                                     <div className='Inter font-normal text-[12px] leading-[14.52px] text-[#525965] dark:text-[#DFDFDF]'>Total Events</div>
                                     <div className='Inter font-[500] text-[14px] leading-[16.94px]'>{item.totalEvents} Events</div>
@@ -75,7 +76,7 @@ function Sports() {
                 </div>
             </div>
             <div className='mt-[50px] flex justify-center'>
-                <div className='w-[124.8px] h-[46px] rounded-[3px] bg-[#2C9CF0] flex items-center justify-center'>
+                <div className='w-[124.8px] h-[46px] rounded-[3px] bg-[#2C9CF0] flex items-center justify-center hover:bg-[#4577c7]'>
                     <div className='text-white text-[14px] Inter font-[600] leading-[25.89px]'>See More</div>
                 </div>
             </div>
